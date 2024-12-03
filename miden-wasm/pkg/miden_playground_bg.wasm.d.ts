90b1e5a7ca0683152dd6d085b8cbf0f004141ddb
Roman <30427286+barabanovro@users.noreply.github.com>
2024-12-03 16:46:15 +0400

    Cloudflare Pages deployment (#5)

---
+++ miden-wasm/pkg/miden_playground_bg.wasm.d.ts
@@ -0,0 +1,29 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_outputs_free(a: number, b: number): void;
+export function __wbg_get_outputs_account_delta_storage(a: number): Array;
+export function __wbg_set_outputs_account_delta_storage(a: number, b: number, c: number): void;
+export function __wbg_get_outputs_account_delta_vault(a: number): Array;
+export function __wbg_set_outputs_account_delta_vault(a: number, b: number, c: number): void;
+export function __wbg_get_outputs_account_delta_nonce(a: number): number;
+export function __wbg_set_outputs_account_delta_nonce(a: number, b: number): void;
+export function __wbg_get_outputs_account_code_commitment(a: number): Array;
+export function __wbg_set_outputs_account_code_commitment(a: number, b: number, c: number): void;
+export function __wbg_get_outputs_account_storage_commitment(a: number): Array;
+export function __wbg_set_outputs_account_storage_commitment(a: number, b: number, c: number): void;
+export function __wbg_get_outputs_account_vault_commitment(a: number): Array;
+export function __wbg_set_outputs_account_vault_commitment(a: number, b: number, c: number): void;
+export function __wbg_get_outputs_account_hash(a: number): Array;
+export function __wbg_set_outputs_account_hash(a: number, b: number, c: number): void;
+export function __wbg_get_outputs_cycle_count(a: number): number;
+export function __wbg_set_outputs_cycle_count(a: number, b: number): void;
+export function __wbg_get_outputs_trace_length(a: number): number;
+export function __wbg_set_outputs_trace_length(a: number, b: number): void;
+export function execute(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number): Array;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __externref_table_dealloc(a: number): void;
+export function __wbindgen_start(): void;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Playground } from '@/components/playground'
 
+export const runtime = 'edge'
+
 export default function Home() {
 	return <Playground />
 }
