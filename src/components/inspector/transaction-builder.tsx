336fee839d79fc4d4a70458c09f48ed5d2a09235
Melnik George <melnik.goshan2013@gmail.com>
2025-01-23 14:09:18 +0300

    Update transaction-builder.tsx

--- src/components/inspector/transaction-builder.tsx
+++ src/components/inspector/transaction-builder.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import InlineIcon from '@/components/ui/inline-icon';
 import { useMiden } from '@/lib/context-providers';
 import {
