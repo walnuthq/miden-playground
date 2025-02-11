fcb4aeef87c6dfcc6224dbcd6a28469f3d39137e
Melnik George <melnik.goshan2013@gmail.com>
2025-02-11 21:40:21 +0300

    Update miden-context-provider.tsx

--- src/lib/context-providers/miden-context-provider.tsx
+++ src/lib/context-providers/miden-context-provider.tsx
@@ -247,19 +247,38 @@ export const MidenContextProvider: React.FC<PropsWithChildren> = ({ children }) 
 			addErrorLog('No notes selected: Please select at least one note to execute the transaction');
 			return;
 		}
+
 		setIsExecutingTransaction(true);
 		const account = accounts[selectedTransactionAccountId];
-		const transactionNotes = selectedTransactionNotesIds.map((noteId) => {
-			const sender = accounts[notes[noteId].senderIdHex];
-			return {
-				note: notes[noteId],
-				noteScript: files[notes[noteId].scriptFileId].content.value!,
-				noteInputs: convertToBigUint64Array(
-					JSON.parse(files[notes[noteId].inputFileId].content.value!)
-				),
+
+		const transactionNotes: {
+			note: Note;
+			noteScript: string;
+			noteInputs: BigUint64Array<ArrayBufferLike>;
+			senderScript: string;
+		}[] = [];
+
+		for (const noteId of selectedTransactionNotesIds) {
+			const note = notes[noteId];
+
+			if (note.isConsumed) {
+				addErrorLog(`${note.name} is already consumed`);
+				setIsExecutingTransaction(false);
+				return;
+			} else {
+				notes[noteId].isConsumed = true;
+			}
+
+			const sender = accounts[note.senderIdHex];
+
+			transactionNotes.push({
+				note,
+				noteScript: files[note.scriptFileId].content.value!,
+				noteInputs: convertToBigUint64Array(JSON.parse(files[note.inputFileId].content.value!)),
 				senderScript: files[sender.scriptFileId].content.value!
-			};
-		});
+			});
+		}
+
 		try {
 			const storage = Account.parseStorage(files[account.storageFileId].content.value!);
 			const output = consumeNotes({
@@ -296,7 +315,6 @@ export const MidenContextProvider: React.FC<PropsWithChildren> = ({ children }) 
 			updateFileById(account.storageFileId, () => {
 				return Account.stringifyStorage(output.storage);
 			});
-
 			addInfoLog('Execution successful');
 			addInfoLog(`Total cycles: ${output.totalCycles}; Trace length: ${output.traceLength}`);
 		} catch (error) {
