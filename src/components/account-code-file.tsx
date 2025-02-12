25022d2b349c49af7cfbe0d46819ec50d0f4f626
Melnik George <melnik.goshan2013@gmail.com>
2025-02-12 17:34:02 +0300

    Update account-code-file.tsx

--- src/components/account-code-file.tsx
+++ src/components/account-code-file.tsx
@@ -1,4 +1,3 @@
-import { cn } from '@/lib/utils';
 import React, { useEffect, useState } from 'react';
 import { CustomMonacoEditor } from './custom-monaco-editor';
 import { useMiden } from '@/lib/context-providers';
@@ -24,69 +23,71 @@ const AccountCodeFile = ({
 	} = useMiden();
 	const [value, setValue] = useState('');
 	const account = accounts[accountId];
-
 	useEffect(() => {
 		setValue(accountFile);
 	}, [accountFile]);
 	return (
 		<div className="relative mx-9 mt-6 text-theme-text">
-			<div className={`relative top-0`}>
-				<div className="flex mb-2 justify-between">
-					<div className="font-bold">Custom component</div>
-				</div>
-				<CustomMonacoEditor
-					value={value}
-					className={cn('h-44 absolute top-0 left-0')}
-					onChange={(value) => {
-						setValue(value ?? '');
-						if (fileId) {
-							updateFileContent(fileId, value ?? '');
-						}
-					}}
-					readOnly={false}
-				/>
-			</div>
-			<div className={`relative top-48`}>
-				<div className="flex mb-2 justify-between">
-					<div className="font-bold">Wallet component</div>
-
-					<ToggleSwitch
-						id="wallet-switch"
-						onToggle={(isEnabled: boolean) =>
-							isEnabled ? enableWalletComponent(accountId) : disableWalletComponent(accountId)
-						}
+			<div className="space-y-6">
+				{' '}
+				<div className="flex flex-col">
+					<div className="flex mb-2 justify-between">
+						<div className="font-bold">Custom component</div>
+					</div>
+					<CustomMonacoEditor
+						value={value}
+						className="h-96"
+						onChange={(value) => {
+							setValue(value ?? '');
+							if (fileId) {
+								updateFileContent(fileId, value ?? '');
+							}
+						}}
+						readOnly={false}
 					/>
 				</div>
-				<CustomMonacoEditor
-					value={ACCOUNT_WALLET_SCRIPT}
-					className={cn(
-						`h-44 absolute top-0 left-0 transition-opacity ease-in-out ${
-							account.isWallet ? '' : 'opacity-25'
-						}`
-					)}
-					readOnly
-				/>
-			</div>
-			<div className={`relative top-96`}>
-				<div className="flex mb-2 justify-between">
-					<div className="font-bold">Auth component</div>
-
-					<ToggleSwitch
-						id="wallet-switch"
-						onToggle={(isEnabled: boolean) =>
-							isEnabled ? enableAuthComponent(accountId) : disableAuthComponent(accountId)
-						}
+				<div className="flex flex-col">
+					<div className="flex mb-2 justify-between">
+						<div className="font-bold">Wallet component</div>
+						<ToggleSwitch
+							id="wallet-switch"
+							onToggle={(isEnabled: boolean) =>
+								isEnabled ? enableWalletComponent(accountId) : disableWalletComponent(accountId)
+							}
+						/>
+					</div>
+					<CustomMonacoEditor
+						value={ACCOUNT_WALLET_SCRIPT}
+						className={`transition-opacity ease-in-out ${account.isWallet ? '' : 'opacity-25'} 
+        ${
+					ACCOUNT_WALLET_SCRIPT.split('\n').length * 18 > 80
+						? 'h-[80px]'
+						: `h-[${ACCOUNT_WALLET_SCRIPT.split('\n').length * 18}px]`
+				}
+				`}
+						readOnly
 					/>
 				</div>
-				<CustomMonacoEditor
-					value={ACCOUNT_AUTH_SCRIPT}
-					className={cn(
-						`h-44 absolute top-0 left-0 transition-opacity ease-in-out ${
-							account.isAuth ? '' : 'opacity-25'
-						}`
-					)}
-					readOnly
-				/>
+				<div className="flex flex-col">
+					<div className="flex mb-2 justify-between">
+						<div className="font-bold">Auth component</div>
+						<ToggleSwitch
+							id="wallet-switch"
+							onToggle={(isEnabled: boolean) =>
+								isEnabled ? enableAuthComponent(accountId) : disableAuthComponent(accountId)
+							}
+						/>
+					</div>
+					<CustomMonacoEditor
+						value={ACCOUNT_AUTH_SCRIPT}
+						className={`${
+							ACCOUNT_AUTH_SCRIPT.split('\n').length * 18 > 80
+								? 'h-[80px]'
+								: `h-[${ACCOUNT_AUTH_SCRIPT.split('\n').length * 18}px]`
+						} transition-opacity ease-in-out ${account.isAuth ? '' : 'opacity-25'}`}
+						readOnly
+					/>
+				</div>
 			</div>
 		</div>
 	);
