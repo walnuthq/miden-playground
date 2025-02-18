d197ac55e76d38ef198dd0325dda2b58f7957a95
Melnik George <91109124+melnikga@users.noreply.github.com>
2025-02-18 17:48:16 +0300

    Fix: Storage file error (#111)

--- src/components/files.tsx
+++ src/components/files.tsx
@@ -1,59 +1,82 @@
 import { useMiden } from '@/lib/context-providers';
 import AccountCodeFile from './account-code-file';
 import { CustomMonacoEditor } from './custom-monaco-editor';
+import { EditorFile } from '@/lib/files';
+import { useEffect, useState } from 'react';
+
+const useSelectedEditorFile = (): { content: string; file: EditorFile | null } => {
+	const { files, accounts, selectedFileId, notes } = useMiden();
+	if (!selectedFileId) return { content: '', file: null };
+	const file = files[selectedFileId];
+	let content = '';
+	if (file) {
+		if (file.content.dynamic) {
+			if (file.content.dynamic.account) {
+				if (file.content.dynamic.account.variant === 'metadata') {
+					const account = accounts[file.content.dynamic.account.accountId];
+					content = JSON.stringify(
+						{
+							accountId: account.id.id.toString()
+						},
+						null,
+						2
+					);
+				} else if (file.content.dynamic.account.variant === 'vault') {
+					const account = accounts[file.content.dynamic.account.accountId];
+					content = JSON.stringify(
+						account.assets.map((asset) => [
+							asset.amount.toString(),
+							0,
+							0,
+							asset.faucetId.toString()
+						]),
+						null,
+						2
+					);
+				}
+			} else if (file.content.dynamic.note) {
+				if (file.content.dynamic.note.variant === 'metadata') {
+					const note = notes[file.content.dynamic.note.noteId];
+					content = JSON.stringify(
+						{
+							senderId: note.senderId.toString(),
+							serialNumber: note.serialNumberDecimalString
+						},
+						null,
+						2
+					);
+				} else if (file.content.dynamic.note.variant === 'vault') {
+					const note = notes[file.content.dynamic.note.noteId];
+					content = JSON.stringify(
+						note.assets.map((asset) => [asset.amount.toString(), 0, 0, asset.faucetId.toString()]),
+						null,
+						2
+					);
+				}
+			}
+		} else {
+			content = file.content.value;
+		}
+	}
+	return { content, file };
+};
 
 export const Files = () => {
-	const { files, accounts, selectedFileId, notes, updateFileContent } = useMiden();
-
-	const file = selectedFileId ? files[selectedFileId] : null;
-
+	const { selectedFileId, updateFileContent } = useMiden();
+	const { content, file } = useSelectedEditorFile();
+	const [value, setValue] = useState(content);
+	useEffect(() => {
+		setValue(content);
+	}, [content]);
 	if (!selectedFileId || !file) return null;
 
-	let content = '';
-	if (file.content.dynamic) {
-		if (file.content.dynamic.account) {
-			if (file.content.dynamic.account.variant === 'metadata') {
-				const account = accounts[file.content.dynamic.account.accountId];
-				content = JSON.stringify(
-					{
-						accountId: account.id.toString()
-					},
-					null,
-					2
-				);
-				return <CustomMonacoEditor readOnly value={content} />;
-			} else if (file.content.dynamic.account.variant === 'vault') {
-				const account = accounts[file.content.dynamic.account.accountId];
-				content = JSON.stringify(
-					account.assets.map((asset) => [asset.amount.toString(), 0, 0, asset.faucetId.toString()]),
-					null,
-					2
-				);
-				return <CustomMonacoEditor readOnly value={content} />;
-			}
-		} else if (file.content.dynamic.note) {
-			if (file.content.dynamic.note.variant === 'metadata') {
-				const note = notes[file.content.dynamic.note.noteId];
-				content = JSON.stringify(
-					{
-						senderId: note.senderId.toString(),
-						serialNumber: note.serialNumberDecimalString
-					},
-					null,
-					2
-				);
-				return <CustomMonacoEditor readOnly value={content} />;
-			} else if (file.content.dynamic.note.variant === 'vault') {
-				const note = notes[file.content.dynamic.note.noteId];
-				content = JSON.stringify(
-					note.assets.map((asset) => [asset.amount.toString(), 0, 0, asset.faucetId.toString()]),
-					null,
-					2
-				);
-				return <CustomMonacoEditor readOnly value={content} />;
-			}
-		}
-	} else if (file.content.variant === 'account-code' && file.content.accountId) {
+	if (
+		file.content &&
+		'variant' in file.content &&
+		file.content.variant === 'account-code' &&
+		'accountId' in file.content &&
+		typeof file.content.accountId === 'string'
+	) {
 		return (
 			<AccountCodeFile
 				accountFile={file.content.value}
@@ -65,12 +88,11 @@ export const Files = () => {
 		return (
 			<CustomMonacoEditor
 				onChange={(value) => {
-					if (file.id) {
-						updateFileContent(file.id, value ?? '');
-					}
+					setValue(value ?? '');
+					if (file && !file.readonly) updateFileContent(file.id, value ?? '');
 				}}
 				readOnly={file.readonly}
-				value={file.content.value}
+				value={value}
 			/>
 		);
 	}
