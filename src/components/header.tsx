79ea60019e8436499c832426e6ad38b201682acd
Melnik George <melnik.goshan2013@gmail.com>
2025-02-19 16:35:22 +0300

    Header fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,21 @@
+import InlineIcon from './ui/inline-icon';
+
 export function Header() {
 	return (
-		<div className="h-full text-theme-text flex justify-between items-center px-12">
-			<div className="font-bold text-2xl">
-				Miden <span className="text-lg p-1.5 bg-white/10 rounded-theme">Playground</span>
+		<div className="h-full text-theme-text flex justify-between items-center">
+			<div className="font-bold text-xl flex gap-2">
+				Miden{' '}
+				<div className="text-xs font-normal text-theme-primary">
+					<span className="bg-theme-border/40 py-1 px-1.5 rounded-full">PLAYGROUND</span>
+				</div>
 			</div>
-			<div className="font-bold text-neutral-400 text-xl cursor-pointer px-2 rounded-theme hover:bg-theme-border">
+			<a
+				href="https://0xpolygonmiden.github.io/miden-vm/"
+				className="font-bold text-theme-text-subtle flex items-center gap-1 text-base cursor-pointer px-2 rounded-theme hover:bg-theme-border"
+			>
 				Documentation
-			</div>
+				<InlineIcon variant="arrow-up-right" className="w-5 h-5" color="gray" />
+			</a>
 		</div>
 	);
 }
