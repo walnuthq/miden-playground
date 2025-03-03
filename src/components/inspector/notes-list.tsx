5454b77e21067563ee0badc54971774bbea2650c
Melnik George <melnik.goshan2013@gmail.com>
2025-03-03 16:36:04 +0300

    Added toasts

--- src/components/inspector/notes-list.tsx
+++ src/components/inspector/notes-list.tsx
@@ -1,6 +1,7 @@
 import { useMiden } from '@/lib/context-providers';
 import React, { useState } from 'react';
 import { FileItem, InspectorItem } from '.';
+import { useToast } from '@/hooks/use-toast';
 
 const NotesList = ({
 	toggleCollapse
@@ -19,11 +20,13 @@ const NotesList = ({
 		createSampleP2IDRNote,
 		createNewNote,
 		createSampleSwapNotes,
-		deleteNote
+		deleteNote,
+		accounts
 	} = useMiden();
 	const [collapsedNotes, setCollapsedNotes] = useState<Record<string, boolean>>({});
 	const isCollapsedNotesTopLevel = collapsedNotes['top-level-notes'] || false;
-
+	const accountsCount = Object.values(accounts).length;
+	const { toast } = useToast();
 	return (
 		<>
 			<InspectorItem
@@ -37,11 +40,35 @@ const NotesList = ({
 				}}
 				onCreate={(option) => {
 					if (option === 'Create P2ID note') {
-						createSampleP2IDNote();
+						if (accountsCount > 1) {
+							createSampleP2IDNote();
+						} else {
+							toast({
+								title: 'Cannot create P2ID note',
+								description: 'To create a P2ID note, you need to create more than one account.',
+								variant: 'destructive'
+							});
+						}
 					} else if (option === 'Create P2IDR note') {
-						createSampleP2IDRNote();
+						if (accountsCount > 1) {
+							createSampleP2IDRNote();
+						} else {
+							toast({
+								title: 'Cannot create P2IDR note',
+								description: 'To create a P2IDR note, you need to create more than one account.',
+								variant: 'destructive'
+							});
+						}
 					} else if (option === 'Create SWAP note') {
-						createSampleSwapNotes();
+						if (accountsCount > 1) {
+							createSampleSwapNotes();
+						} else {
+							toast({
+								title: 'Cannot create SWAP note',
+								description: 'To create a SWAP note, you need to create more than one account.',
+								variant: 'destructive'
+							});
+						}
 					} else if (option === 'Create empty note') {
 						createNewNote();
 					}
