b5738b17b995a61cead1ebaa1f85cad27d5bdf7a
Melnik George <melnik.goshan2013@gmail.com>
2025-03-07 11:05:33 +0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const Footer = () => {
 	return (
-		<div className="flex justify-end">
-			<div className="text-theme-text-subtle text-xs mt-2">
+		<div className="">
+			<div className="text-theme-text-subtle text-sm mt-2">
 				Built with ❤️ by{' '}
 				<Link
 					href="https://github.com/walnuthq/miden-playground"
-					className="text-theme-primary hover:underline"
+					className="underline"
 					target="_blank"
 				>
 					Walnut
