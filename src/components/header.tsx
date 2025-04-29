fcb4b9358a09f6336e1b36c9ec87ec02616d9fdc
Melnik George <91109124+melnikga@users.noreply.github.com>
2025-04-29 13:11:13 +0300

    New logo and colors (#170)
    * New logo and colors
    
    * Update globals.css
    
    * Update favicon.ico
    
    * made logo smaller

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,13 @@
 import InlineIcon from './ui/inline-icon';
 import Image from 'next/image';
-import logo from '../app/images/miden_logo.png';
+import logo from '../app/images/miden_logo.svg';
 import Link from 'next/link';
 
 export function Header() {
 	return (
 		<div className="h-full text-theme-text flex justify-between items-center">
 			<div className="font-bold text-xl flex items-center gap-1">
-				<Image src={logo} alt="logo" className="w-10 h-10" />
+				<Image src={logo} alt="logo" className="w-6 h-6" />
 				Miden{' '}
 				<div className="text-xs font-normal text-theme-primary">
 					<span className="bg-theme-border/40 py-1 px-1.5 ml-2 rounded-full">PLAYGROUND</span>
