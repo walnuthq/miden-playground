d438c404c45b740896980a161b0e87c3917360e3
saimeunt <saimeunt@gmail.com>
2025-12-08 17:16:07 +0100

    fix build

--- apps/web/app/api/route.ts
+++ apps/web/app/api/route.ts
@@ -4,7 +4,6 @@ export const GET = async () =>
   NextResponse.json({
     timestamp: Date.now(),
     env: {
-      ENVIRONMENT: process.env.ENVIRONMENT,
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
   });
