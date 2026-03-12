b43e389d4cdfb783331920b3d92a601f8eeb53f7
saimeunt <saimeunt@gmail.com>
2026-03-12 01:46:26 +0100

    switch from proxy to middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export const proxy = () => {
+export const middleware = () => {
   if (process.env.NODE_ENV === "production") {
     return NextResponse.rewrite(
       "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.e00a052b.wasm",
