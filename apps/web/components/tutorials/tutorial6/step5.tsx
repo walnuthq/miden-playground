f78967aab32d0ac7fe5a5d0bc9d8d66d6e9bff7e
saimeunt <saimeunt@gmail.com>
2026-03-16 10:32:37 +0100

    fix counter tutorials

--- apps/web/components/tutorials/tutorial6/step5.tsx
+++ apps/web/components/tutorials/tutorial6/step5.tsx
@@ -10,7 +10,7 @@ const useCompleted = () => {
   const { components } = useComponents();
   const component = components.find(
     ({ type, scriptId }) =>
-      type === "account" && scriptId.startsWith("counter-contract_"),
+      type === "account" && scriptId.startsWith("counter-account_"),
   );
   const counter = accounts.find(({ components }) =>
     components.includes(component?.id ?? ""),
